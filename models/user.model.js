a26896a655361592f364143d3c377e865a2de9ec
efrat-burshteyn <efrat4687@gmail.com>
2026-08-02 01:10:27 +0300

    part 7

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,7 @@
 
 import { model,Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import React from "react";
 const arrLength =(arr)=>{
     return arr.length<=3;
 }
@@ -32,5 +34,33 @@ const arrLength =(arr)=>{
             validate: [arrLength,'אי אפשר להשאיל יותר משלושה ספרים!']
         }
     });
+    userSchema.pre("save",async function (next){
+        if(!this.isModified("password"))
+            return next();
+        try{
+            const salt =await bcrypt.genSalt(10);
+            this.password=await bcrypt.hash(this.password,salt);
+            next();
+        }
+        catch(error){
+            next(error);
+        }
+    } );
+    userSchema.statics.checkPassword=async function (email,candidatePassword){
+        const user = await this.findOne({email});
+        if(!user){
+            return false;
+        }
+        const isMatch = await bcrypt.compare(candidatePassword,user.password);
+        return isMatch ? user : false;
+    };
+    userSchema.set('toJSON',{
+        Transform: function (doc,ret){
+            ret.id=ret._id;
+            delete ret._id;
+            delete ret.password;
+            return ret;
+        }
+    })
     export const User = model("User",userSchema);
       
